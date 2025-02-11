d725b2e92b4861f59a5429b409023dbec8768b28
xaviertingxuan <xaviertingxuan@gmail.com>
2025-02-11 12:30:20 +0800

    v0.03 setup backend

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from '@/App'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { api } from '@/state/api'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+})
+setupListeners(store.dispatch);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
