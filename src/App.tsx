50afd682f2aea7e639e824dd9c2fea982cdc6c68
xaviertingxuan <xaviertingxuan@gmail.com>
2025-02-11 11:07:06 +0800

    v0.02

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import './App.css'
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
+import Navbar from "./scenes/navbar/navbar";
+
+import Dashboard from "@/scenes/dashboard";
+// import Predictions from "@/scenes/predictions";
 
 function App() {
-
-
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <>
-     <div className="app"></div>
-    </>
-  )
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<div>prediction page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
